a6dbd5beec152108dd70cbdbbbd8a952a4748ee7
cachen-hi <cachen@smls.on.ca>
2026-06-04 12:53:42 -0400

    deleted unused import and changed the name the Scan picking screen

--- AISuite_Demos/AIDataCaptureDemo/app/src/main/java/com/zebra/aidatacapturedemo/ui/view/BarcodeMapScanPickingScreen.kt
+++ AISuite_Demos/AIDataCaptureDemo/app/src/main/java/com/zebra/aidatacapturedemo/ui/view/BarcodeMapScanPickingScreen.kt
@@ -17,8 +17,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.zebra.aidatacapturedemo.data.AIDataCaptureDemoUiState
-import com.zebra.aidatacapturedemo.data.ProductInfo
 import com.zebra.aidatacapturedemo.viewmodel.AIDataCaptureDemoViewModel
 
 @Composable
